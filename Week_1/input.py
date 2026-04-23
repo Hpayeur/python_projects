13229fc2f15862947dcd8f84dd7f023431046e61
Hpayeur <hailpaye017@gmail.com>
2026-04-23 17:46:13 -0600

    Week One

---
+++ Week_1/input.py
@@ -0,0 +1,16 @@
+# Functions with more than 1 input
+# def greet_with_name(name):
+#     print(f"Hello, {name}!")
+#     print("How do you do {name}?")
+# greet_with_name("Jack Stones")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+    # greet_with("Jack Knox", "Nowhere")
+
+
+    # greet_with("Nowhere", "Jack Knox")
+
+    greet_with(name="Angela", location="London")
---
+++ Week_1/output.py
@@ -0,0 +1,22 @@
+# Functions with Outputs
+def function_1(text):
+    return text + text
+
+def function_2(text):
+    return text.title()
+
+output = function_1("hello")
+print(output)
+
+    # def format_name(f_name, l_name):
+    #     print(f_name.title())
+    #     print(l_name.title())
+    #     format_name("angela", "ANGELA")
+
+   #  def format_name(f_name, l_name):
+   #      formated_f_name = f_name.title()
+   #      formated_l_name = l_name.title()
+   #      return f"{formated_f_name} {formated_l_name}"
+   #  print(format_name("AnGeLa", "Yu"))
+   #
+   # output = len("Angela")
